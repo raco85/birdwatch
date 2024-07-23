309ef2486953dbb60c2dc28e06613171881ef3e4
DESKTOP-FM46T92\radomir.drazic <radomir.drazic@eng.it>
2024-07-23 15:26:59 +0200

    Create project setup

---
+++ src/main/java/com/radomir/drazic/birdwatchingapp/BirdwatchingappApplication.java
@@ -0,0 +1,13 @@
+package com.radomir.drazic.birdwatchingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdwatchingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdwatchingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radomir/drazic/birdwatchingapp/BirdwatchingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radomir.drazic.birdwatchingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdwatchingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
